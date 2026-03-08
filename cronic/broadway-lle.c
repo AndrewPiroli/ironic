c2f1d3fb3a05a78cb59273e61c87f91ac0d72a67
techflashYT <officialTechflashYT@gmail.com>
2026-03-07 19:18:42 -0700

    Cronic: add Broadway LLE w/ libppcemu+Cronic

---
+++ cronic/broadway-lle.c
@@ -0,0 +1,591 @@
+/*
+ * Ironic - Broadway Emulation
+ *
+ * Copyright (C) 2025 - 2026 Techflash
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ppcemu/ppcemu.h>
+#include "cronic.h"
+
+/*#define DEBUG_BUS*/
+#define NUM_BREAKPOINTS 16
+
+static bool previouslyRunning = false;
+static bool running = false;
+static bool started = false;
+static bool singleStep = false;
+static uint32_t breakpoints[NUM_BREAKPOINTS];
+static bool breakpointsActive[NUM_BREAKPOINTS];
+static bool continuePastBP = false;
+static char *debuggerReason = NULL;
+static uint32_t dbgOldGPRs[32];
+static uint32_t dbgOldPC, dbgOldLR, dbgOldCTR, dbgOldCR, dbgOldXER, dbgOldMSR;
+
+static void bus_hook(struct ppcemu_state *emu, uint32_t addr, unsigned int len, void *data, bool is_write) {
+	/* emu, addr, len, the pointer to data, and is_write are host-endian; the bytes pointed to by data are big-endian */
+#ifdef DEBUG_BUS
+	printf("Got bus transaction (%s) for %08x, %dB", is_write ? "write" : "read", addr, len);
+	if (is_write) {
+		printf(", data ");
+		if (len == 1)
+			printf("%02x\r\n", *(uint8_t *)data);
+		else if (len == 2)
+			printf("%04x\r\n", ppcemu_be16_to_cpu(*(uint16_t *)data));
+		else if (len == 4)
+			printf("%08x\r\n", ppcemu_be32_to_cpu(*(uint32_t *)data));
+	}
+	else
+		puts("");
+#endif
+
+	/* Ironic returns already-big-endian data from the IPC interface, but we need to swap for writes */
+
+	if (addr >= 0xfff00100 && addr <= 0xfff00200 && len == 4) { /* special case for EXI boot vector alias */
+		if (is_write)
+			return;
+		else
+			*(uint32_t *)data = IPC_Read32((addr - 0xfff00100) + 0x0d806840);
+	}
+	else if ((addr >= 0x00000000 && addr <= 0x017ffffff) || (addr >= 0x0c000000 && addr <= 0xdfffffff) || (addr >= 0x10000000 && addr <= 0x13ffffff)) {
+		if (len == 1) {
+			if (is_write)
+				IPC_Write8(addr, *(uint8_t *)data);
+			else
+				*(uint8_t *)data = IPC_Read8(addr);
+		}
+		else if (len == 2) {
+			if (is_write)
+				IPC_Write16(addr, ppcemu_be16_to_le16(*(uint16_t *)data));
+			else
+				*(uint16_t *)data = IPC_Read16(addr);
+		}
+		else if (len == 4) {
+			if (is_write)
+				IPC_Write32(addr, ppcemu_be32_to_le32(*(uint32_t *)data));
+			else
+				*(uint32_t *)data = IPC_Read32(addr);
+		}
+	}
+	else {
+		printf("Unknown address: 0x%08x\r\n", addr);
+		exit(1);
+	}
+}
+
+static void ctrlcHandler(int sig) {
+	(void)sig;
+	running = false;
+	debuggerReason = "Ctrl-C";
+}
+
+static void __attribute__((noreturn)) cleanup(int ret) {
+	/* TODO: ppcemu_cleanup once I write that */
+	IPC_Cleanup();
+	exit(ret);
+}
+
+static int dbgAddBP(uint32_t addr) {
+	int i;
+
+	for (i = 0; i < NUM_BREAKPOINTS; i++) {
+		if (!breakpointsActive[i]) {
+			breakpoints[i] = addr;
+			breakpointsActive[i] = true;
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+static int dbgDelBP(uint32_t addr) {
+	int i;
+
+	for (i = 0; i < NUM_BREAKPOINTS; i++) {
+		if (breakpointsActive[i] && breakpoints[i] == addr) {
+			breakpoints[i] = 0;
+			breakpointsActive[i] = false;
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+
+static void dbgHelp(void) {
+	puts(
+"Ironic Broadway LLE\r\n\
+\r\n\
+Commands:\r\n\
+	help			Display this help message.\r\n\
+\r\n\
+	q, quit			Quit the emulator (does not quit Ironic).\r\n\
+\r\n\
+	info [target]		Get info about something.\r\n\
+		regs, registers		Dump the CPU registers.\r\n\
+		spr [name/num]		Dump the SPR at index [num] or\r\n\
+					with name [name].\r\n\
+\r\n\
+	set [target]		Set something.\r\n\
+		gpr [idx] [val]	Set GPR r[idx] to [val].\r\n\
+		spr [idx] [val]	Set SPR r[idx] to [val].\r\n\
+\r\n\
+	b, break [addr]		Set a breakpoint at [addr].\r\n\
+\r\n\
+	bd [addr]		Delete the breakpoint at [addr].\r\n\
+\r\n\
+	bl			List created breakpoints.\r\n\
+\r\n\
+	r, run			Start the emulator from Hard Reset and\r\n\
+				run indefinitely, or until a breakpoint\r\n\
+				is hit.\r\n\
+\r\n\
+	c, cont, continue	Continue emulation indefinitely, or\r\n\
+				until a breakpoint is hit.\r\n\
+\r\n\
+	s, step			Step forward one instruction.\r\n\
+\r\n\
+	log [source] [level]	Set logging level for [source] to [level],\r\n\
+				where [source] is one of \"translation\",\r\n\
+				\"ifetch\",\"decode\", \"branch\",\r\n\
+				\"loadstore\", or \"misc\", and [level]\r\n\
+				is one of \"debug\", \"verbose\", \"info\",\r\n\
+				\"warn\", or \"error\".");
+}
+
+
+static bool dbgConfirm(const char *msg, bool autoYes) {
+	char *conf = NULL;
+	size_t size = 0;
+	ssize_t ret;
+
+	while (true) {
+		printf("%s %s ", msg, autoYes ? "[Y/n]" : "[N/y]");
+		ret = getline(&conf, &size, stdin);
+
+		if (ret == -1)
+			cleanup(1);
+
+		/* trim trailing newlines and spaces */
+		size--;
+		while (size > 0 && (conf[size] == '\r' || conf[size] == '\n' || conf[size] == ' ' || conf[size] == '\0')) {
+			conf[size] = '\0';
+			size--;
+		}
+
+		if (!strcmp(conf, "yes") || !strcmp(conf, "y") || !strcmp(conf, "Y") || !strcmp(conf, "Yes") || !strcmp(conf, "YES") || (!strcmp(conf, "") && autoYes)) {
+			free(conf);
+			return true;
+		}
+		else if (!strcmp(conf, "no") || !strcmp(conf, "n") || !strcmp(conf, "N") || !strcmp(conf, "No") || !strcmp(conf, "NO") || (!strcmp(conf, "") && !autoYes)) {
+			free(conf);
+			return false;
+		}
+		else {
+			puts("Invalid response, please try again.");
+			free(conf);
+		}
+	}
+
+	free(conf);
+}
+
+static void dbgPrintReg(const char *prefix, uint32_t new, uint32_t old) {
+	if (new != old)
+		printf("%s\x1b[1;31m[%08x]\x1b[0m", prefix, new);
+	else
+		printf("%s\x1b[0m %08x ", prefix, new);
+}
+
+
+static void dbgRegDump(struct ppcemu_state *emu) {
+	uint32_t gpr[32];
+	uint64_t fpr[32];
+	uint32_t pc, msr, lr, xer, cr, ctr;
+	int i;
+
+	for (i = 0; i < 32; i++)
+		gpr[i] = ppcemu_get_gpr(emu, i);
+
+	pc = ppcemu_get_pc(emu);
+	msr = ppcemu_get_msr(emu);
+	lr = ppcemu_get_spr(emu, PPCEMU_SPRN_LR);
+	xer = ppcemu_get_spr(emu, PPCEMU_SPRN_XER);
+	ctr = ppcemu_get_spr(emu, PPCEMU_SPRN_CTR);
+	cr = ppcemu_get_cr(emu);
+
+	puts("GPRs:");
+	puts("     r0-r7         r8-r15        r16-r23       r24-r31");
+	for (i = 0; i < 8; i++) {
+		dbgPrintReg("    ", gpr[i], dbgOldGPRs[i]);
+		dbgPrintReg("    ", gpr[8 + i], dbgOldGPRs[8 + i]);
+		dbgPrintReg("    ", gpr[16 + i], dbgOldGPRs[16 + i]);
+		dbgPrintReg("    ", gpr[24 + i], dbgOldGPRs[24 + i]);
+		puts("");
+	}
+
+	memcpy(dbgOldGPRs, gpr, sizeof(gpr));
+
+	puts("");
+	dbgPrintReg("PC: ", pc, dbgOldPC);
+	dbgPrintReg("    MSR: ", msr, dbgOldMSR);
+	puts("");
+	dbgPrintReg("LR: ", lr, dbgOldLR);
+	dbgPrintReg("    XER: ", xer, dbgOldXER);
+	puts("");
+	dbgPrintReg("CR: ", cr, dbgOldCR);
+	dbgPrintReg("    CTR: ", ctr, dbgOldCTR);
+	puts("");
+
+	dbgOldPC = pc;
+	dbgOldMSR = msr;
+	dbgOldLR = lr;
+	dbgOldXER = xer;
+	dbgOldCR = cr;
+	dbgOldCTR = ctr;
+}
+
+static void debugger(struct ppcemu_state *emu) {
+	char *cmd = NULL, *arg = NULL, *tmp;
+	size_t cmdSize = 0;
+	ssize_t ret;
+	bool confirmed;
+	uint32_t addr, val;
+	int i, idx;
+	enum ppcemu_loglevel level;
+	enum ppcemu_log_source source;
+
+	if (previouslyRunning && !singleStep)
+		printf("Broke into debugger from %s at PC=0x%08x\r\n", debuggerReason, ppcemu_get_pc(emu));
+
+	printf("(ppcdbg) ");
+	ret = getline(&cmd, &cmdSize, stdin);
+	
+	if (ret == -1)
+		cleanup(1);
+
+	/* trim trailing newlines and spaces */
+	cmdSize--;
+	while (cmdSize > 0 && (cmd[cmdSize] == '\r' || cmd[cmdSize] == '\n' || cmd[cmdSize] == ' ') || cmd[cmdSize] == '\0') {
+		cmd[cmdSize] = '\0';
+		cmdSize--;
+	}
+
+	/* split by space */
+	strtok(cmd, " ");
+
+	if (!strcmp(cmd, "help"))
+		dbgHelp();
+	else if (!strcmp(cmd, "q") || !strcmp(cmd, "quit")) {
+		free(cmd);
+		cleanup(0);
+	}
+	else if (!strcmp(cmd, "info")) {
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing info target argument");
+			goto out;
+		}
+		else if (!strcmp(arg, "regs") || !strcmp(arg, "registers"))
+			dbgRegDump(emu);
+		else if (!strcmp(arg, "spr")) {
+			arg = strtok(NULL, " ");
+			if (!arg) {
+				puts("Missing SPR name/number argument");
+				goto out;
+			}
+
+			if (!strcasecmp(arg, "XER"))
+				idx = PPCEMU_SPRN_XER;
+			else if (!strcasecmp(arg, "LR"))
+				idx = PPCEMU_SPRN_LR;
+			else if (!strcasecmp(arg, "CTR"))
+				idx = PPCEMU_SPRN_CTR;
+			else if (!strcasecmp(arg, "HID0"))
+				idx = PPCEMU_SPRN_HID0;
+			else if (!strcasecmp(arg, "HID2"))
+				idx = PPCEMU_SPRN_HID2_GEKKO;
+			else if (!strcasecmp(arg, "HID4"))
+				idx = PPCEMU_SPRN_HID4;
+			else if (!strcasecmp(arg, "SRR0"))
+				idx = PPCEMU_SPRN_SRR0;
+			else if (!strcasecmp(arg, "SRR1"))
+				idx = PPCEMU_SPRN_SRR1;
+			else {
+				idx = strtoul(arg, &tmp, 10);
+				if (tmp == arg || idx > 1024) {
+					printf("Invalid SPR number: %s\r\n", arg);
+					goto out;
+				}
+			}
+
+			val = ppcemu_get_spr(emu, idx);
+			printf("SPR %d: %08x\r\n", idx, val);
+		}
+		else
+			printf("Unknown info target: %s\r\n", arg);
+	}
+	else if (!strcmp(cmd, "set")) {
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing set target argument");
+			goto out;
+		}
+		else if (!strcmp(arg, "gpr")) {
+			arg = strtok(NULL, " ");
+			if (!arg) {
+				puts("Missing GPR index argument");
+				goto out;
+			}
+
+			if (arg[0] == 'r')
+				arg++;
+
+			idx = strtoul(arg, &tmp, 10);
+			if (tmp == arg || idx > 31) {
+				printf("Invalid GPR number: %s\r\n", arg);
+				goto out;
+			}
+
+			arg = strtok(NULL, " ");
+			if (!arg) {
+				puts("Missing value argument");
+				goto out;
+			}
+
+			val = strtoul(arg, &tmp, 16);
+			if (tmp == arg || val > 0xffffffff) {
+				printf("Invalid GPR value: %s\r\n", arg);
+				goto out;
+			}
+
+			ppcemu_set_gpr(emu, idx, val);
+		}
+		else if (!strcmp(arg, "spr")) {
+			arg = strtok(NULL, " ");
+			if (!arg) {
+				puts("Missing SPR index argument");
+				goto out;
+			}
+
+			idx = strtoul(arg, &tmp, 10);
+			if (tmp == arg || idx > 1024) {
+				printf("Invalid SPR number: %s\r\n", arg);
+				goto out;
+			}
+
+			arg = strtok(NULL, " ");
+			if (!arg) {
+				puts("Missing value argument");
+				goto out;
+			}
+
+			val = strtoul(arg, &tmp, 16);
+			if (tmp == arg || val > 0xffffffff) {
+				printf("Invalid SPR value: %s\r\n", arg);
+				goto out;
+			}
+
+			ppcemu_set_spr(emu, idx, val);
+		}
+		else
+			printf("Unknown set target: %s\r\n", arg);
+	}
+	else if (!strcmp(cmd, "b") || !strcmp(cmd, "break")) {
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing address");
+			goto out;
+		}
+
+		addr = strtoul(arg, &tmp, 16);
+		if (tmp == arg || addr > 0xffffffff)
+			printf("Invalid breakpoint address: %s\r\n", arg);
+		else {
+			idx = dbgAddBP(addr);
+			if (idx == -1)
+				puts("Could not add breakpoint");
+			else
+				printf("Added breakpoint %d for PC=0x%08x\r\n", idx, addr);
+		}
+
+	}
+	else if (!strcmp(cmd, "bl")) {
+		for (i = 0; i < NUM_BREAKPOINTS; i++) {
+			if (breakpointsActive[i])
+				printf("Breakpoint %d: 0x%08x\r\n", breakpoints[i]);
+		}
+	}
+	else if (!strcmp(cmd, "bd")) {
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing address");
+			goto out;
+		}
+
+		addr = strtoul(arg, &tmp, 16);
+		if (tmp == arg || addr > 0xffffffff)
+			printf("Invalid breakpoint address: %s\r\n", arg);
+		else {
+			idx = dbgDelBP(addr);
+			if (idx == -1)
+				printf("There is no breakpoint at PC=0x%08x\r\n", addr);
+			else
+				printf("Deleted breakpoint %d @ PC=0x%08x\r\n", idx, addr);
+		}
+	}
+	else if (!strcmp(cmd, "log")) {
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing log source");
+			goto out;
+		}
+
+		if (!strcmp(arg, "translation"))
+			source = PPCEMU_LOG_SOURCE_ADDR_TRANSLATION;
+		else if (!strcmp(arg, "ifetch"))
+			source = PPCEMU_LOG_SOURCE_IFETCH;
+		else if (!strcmp(arg, "decode"))
+			source = PPCEMU_LOG_SOURCE_INSTR_DECODE;
+		else if (!strcmp(arg, "branch"))
+			source = PPCEMU_LOG_SOURCE_BRANCH;
+		else if (!strcmp(arg, "loadstore"))
+			source = PPCEMU_LOG_SOURCE_LOADSTORE;
+		else if (!strcmp(arg, "misc"))
+			source = PPCEMU_LOG_SOURCE_MISC;
+		else {
+			printf("Unknown log source: %s\r\n", arg);
+			goto out;
+		}
+
+
+		arg = strtok(NULL, " ");
+		if (!arg) {
+			puts("Missing log level");
+			goto out;
+		}
+
+		if (!strcmp(arg, "debug"))
+			level = PPCEMU_LOGLEVEL_DEBUG;
+		else if (!strcmp(arg, "verbose"))
+			level = PPCEMU_LOGLEVEL_VERBOSE;
+		else if (!strcmp(arg, "info"))
+			level = PPCEMU_LOGLEVEL_INFO;
+		else if (!strcmp(arg, "warn"))
+			level = PPCEMU_LOGLEVEL_WARN;
+		else if (!strcmp(arg, "error"))
+			level = PPCEMU_LOGLEVEL_ERROR;
+		else {
+			printf("Unknown log level: %s\r\n", arg);
+			goto out;
+		}
+
+		ppcemu_set_loglevel(source, level);
+	}
+	else if (!strcmp(cmd, "r") || !strcmp(cmd, "run")) {
+		if (started) {
+			confirmed = dbgConfirm("The emulated Broadway is already running, would you like to restart it?", true);
+			if (confirmed) {
+				/* TODO: reset */
+				running = true;
+			}
+		}
+		else {
+			running = true;
+			started = true;
+			singleStep = false;
+		}
+	}
+	else if (!strcmp(cmd, "c") || !strcmp(cmd, "cont") || !strcmp(cmd, "continue")) {
+		if (started) {
+			running = true;
+			continuePastBP = true;
+			singleStep = false;
+		}
+		else
+			puts("The emulated Broadway is not running, cannot continue.");
+	}
+	else if (!strcmp(cmd, "s") || !strcmp(cmd, "step")) {
+		running = true;
+		singleStep = true;
+		continuePastBP = true;
+	}
+	else
+		printf("Unknown command: %s.  Try \"help\" for help.\r\n", cmd);
+
+out:
+	free(cmd);
+}
+
+static void emuStep(struct ppcemu_state *emu) {
+	uint32_t pc;
+	int i;
+
+	/* check for breakpoints */
+	if (!continuePastBP) {
+		pc = ppcemu_get_pc(emu);
+
+		for (i = 0; i < NUM_BREAKPOINTS; i++) {
+			if (pc == breakpoints[i] && breakpointsActive[i]) { /* breakpoint hit */
+				running = false;
+				debuggerReason = "breakpoint";
+				return;
+			}
+		}
+	}
+
+	continuePastBP = false;
+
+	if (singleStep)
+		running = false;
+
+	/* actually run an instruction */
+	ppcemu_step(emu);
+}
+
+int main(void) {
+	struct ppcemu_state *emu;
+	bool tmpRunning;
+
+	emu = ppcemu_init(PPCEMU_CPU_MODEL_BROADWAY, bus_hook, 243000, 3);
+	if (!emu) {
+		puts("libppcemu init failed");
+		return 1;
+	}
+
+	if (IPC_Init()) {
+		puts("cronic init failed");
+//		return 1;
+	}
+
+	/* to break into the debugger */
+	signal(SIGINT, ctrlcHandler);
+
+	/* clean up debugger state */
+	memset(breakpoints, 0, sizeof(breakpoints));
+	memset(breakpointsActive, 0, sizeof(breakpointsActive));
+	memset(dbgOldGPRs, 0, sizeof(dbgOldGPRs));
+	dbgOldPC = dbgOldLR = dbgOldCTR = dbgOldCR = dbgOldXER = dbgOldMSR = 0;
+
+	while (true) {
+		if (IPC_Err) {
+			printf("Hit IPC error @ PC=0x%08x\r\n", ppcemu_get_pc(emu));
+			cleanup(1);
+		}
+
+		tmpRunning = running;
+
+		if (running)
+			emuStep(emu);
+		else
+			debugger(emu);
+
+		previouslyRunning = tmpRunning;
+	}
+}
